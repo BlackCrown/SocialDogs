30efdadaa8291343effe0f17e35f26142784372d
Wagner Araujo <wagnercorp17@gmail.com>
2024-11-12 20:53:39 -0300

    componente de Input e Button criados e estilizados, prontos para serem importados

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Input from '../Form/Input.jsx';
+import Button from '../Form/Button.jsx';
 
 const LoginForm = () => {
   const [userName, setUserName] = React.useState('');
@@ -25,19 +27,15 @@ const LoginForm = () => {
 
   return (
     <section>
-      <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={userName}
-          onChange={({ target }) => setUserName(target.value)}
-        />
-        <input
-          type="text"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <button>Logar</button>
+        <fieldset>
+          <legend>
+            <h1>Login</h1>
+          </legend>
+          <Input label="Usuário" type="text" name="userName" />
+          <Input label="Senha" type="password" name="password" />
+          <Button>Logar</Button>
+        </fieldset>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
