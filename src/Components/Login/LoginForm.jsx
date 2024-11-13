6a69c79d462f4c62ed4a95f04f2146007027317d
Wagner Araujo <wagnercorp17@gmail.com>
2024-11-13 20:00:15 -0300

    LoginForm atualizado com validacao de dados e menssagens de erro, criado arquivo de React hook para os formularios

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,27 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../Form/Input.jsx';
 import Button from '../Form/Button.jsx';
+import useForm from '../../Hooks/useForm.jsx';
 
 const LoginForm = () => {
-  const [userName, setUserName] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const userName = useForm();
+  const password = useForm();
+  console.log(userName);
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ userName, password }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
+
+    if (userName.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userName, password }),
       })
-      .then((json) => {
-        console.log(json);
-      });
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
 
   return (
@@ -32,8 +37,8 @@ const LoginForm = () => {
           <legend>
             <h1>Login</h1>
           </legend>
-          <Input label="Usuário" type="text" name="userName" />
-          <Input label="Senha" type="password" name="password" />
+          <Input label="Usuário" type="text" name="userName" {...userName} />
+          <Input label="Senha" type="password" name="password" {...password} />
           <Button>Logar</Button>
         </fieldset>
       </form>
