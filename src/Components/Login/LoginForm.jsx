b5fd355b349122c2da4d32eb5eb0c8ca87414fe4
Wagner Araujo <wagnercorp17@gmail.com>
2024-11-18 22:22:36 -0300

    criada api fetch para uso em todo aplicativo, funcao para bucar usuario cadastrado no app (esta com erro)

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,30 +3,38 @@ import { Link } from 'react-router-dom';
 import Input from '../Form/Input.jsx';
 import Button from '../Form/Button.jsx';
 import useForm from '../../Hooks/useForm.jsx';
+import { TOKEN_POST, USER_GET } from '../../api.jsx';
 
 const LoginForm = () => {
   const userName = useForm();
   const password = useForm();
-  console.log(userName);
 
-  function handleSubmit(event) {
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = response.json();
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (userName.validate() && password.validate()) {
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ userName, password }),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-        });
+      const { url, options } = TOKEN_POST({
+        username: userName.value,
+        password: password.value,
+      });
+      const response = await fetch(url, options);
+      const json = await response.json();
+
+      window.localStorage.setItem('token', json.token);
+      getUser(json.token);
     }
   }
 
