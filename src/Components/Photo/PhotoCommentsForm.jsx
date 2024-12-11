63393e07743f2d5f8c97c98ab20a357e68b3ced6
Wagner Araujo <wagnercorp17@gmail.com>
2024-12-10 22:40:18 -0300

    Secao de comentarios com scroll automatico para o comentario mais recente

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Enviar from '../../Assets/enviar.svg?react';
 import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 import { COMMENT_POST } from '../../api';
+import styles from './PhotoCommentsForm.module.css';
 
 const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('');
@@ -10,24 +12,27 @@ const PhotoCommentsForm = ({ id, setComments }) => {
   async function handleSubmit(event) {
     event.preventDefault();
     const { url, options } = COMMENT_POST(id, { comment });
-    const { response } = await request(url, options);
+    const { response, json } = await request(url, options);
     if (response.ok) {
-      setComments((comments) => [...comments, comment]);
+      setComment('');
+      setComments((comments) => [...comments, json]);
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Auau..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
+      <Error error={error} />
     </form>
   );
 };
