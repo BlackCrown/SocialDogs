e7b38ad5134e5b47635e2abea1f162d0b2dc9135
Wagner Araujo <wagnercorp17@gmail.com>
2024-12-11 22:30:19 -0300

    Botao deletar foto e logica do mesmo criados e funcionando normalmente

--- src/Components/Photo/PhotoDelete.jsx
+++ src/Components/Photo/PhotoDelete.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import styles from './PhotoDelete.module.css';
+import { PHOTO_DELETE } from '../../api';
+import useFetch from '../../Hooks/useFetch';
 
-const PhotoDelete = () => {
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick(event) {
+    const confirm = window.confirm(
+      'A foto será deletada, junto com todos os comentários, deseja continuar???',
+    );
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) {
+        window.location.reload();
+      }
+    }
+  }
   return (
-    <div>
-      <button className={styles.delete}>Deletar</button>
-    </div>
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletando...
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
   );
 };
 
