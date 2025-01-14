ec37e34db66ad362c75dfea495f56bb1765d48a9
Wagner Araujo <wagnercorp17@gmail.com>
2025-01-14 13:47:26 -0300

    Criado grafico de pizza com uso da API Victory, mostrando o total de acesso em cada foto do usuário logado

--- src/Components/User/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from './UserStatsGraphs.module.css';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
 
 const UserStatsGraphs = ({ data }) => {
   const [graph, setGraph] = React.useState([]);
   const [total, setTotal] = React.useState(0);
 
   React.useEffect(() => {
+    const graphData = data.map((item) => {
+      return {
+        x: item.title,
+        y: Number(item.acessos),
+      };
+    });
+    setGraph(graphData);
     setTotal(
       data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b, 0),
     );
@@ -14,6 +22,9 @@ const UserStatsGraphs = ({ data }) => {
     <div className={`${styles.graph} animeLeft`}>
       <div className={styles.total}>
         <p>Acessos: {total}</p>
+      </div>
+      <div>
+        <VictoryPie data={graph} />
       </div>
     </div>
   );
